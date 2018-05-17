1c6aaa409d8076c0b7e2229aca21d06127f0de76
Fluently <f_liuchang@163.com>
2018-05-17 22:23:25 +0800

    TimeCard Completed

--- timeCard.js
+++ timeCard.js
@@ -1,156 +1,162 @@
-(function() {
-  // 初始化select下拉框年份，默认从当前往前5年
-  var date = new Date();
-  for (var i = date.getFullYear(); i > date.getFullYear() - 5; i--) {
-    $("#selectYear").append("<option value=" + i + ">" + i + "</option>")
-  }
+// 日历对象
+function Calendar() {
+  // 日历基础配置，可扩展
+  this.config = {
+      startYear: 2013,
+      endYear: new Date().getFullYear(),
+      eleYear: "#selectYear",
+      constantMonth: ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"],
+      eleMonth: "#selectMonth",
+      constantWeek: ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"],
+      eleWeek: ".week"
+    },
+    this.date = new Date(),
+    this.preDateInfo = [],
+    this.curDateInfo = [],
+    this.afterDateInfo = [],
+    this.daysSize = 0,
+    this.firstDayOfWeek = 0,
+    this.lastDayOfWeek = 0
+}
 
-  //初始化左侧月份选择
-  for (var i = 0; i < monthArray.length;) {
-    $("#selectMonth").append("<div>" +
-      "<span id = " + monthArray.slice(i, i + 2)[0] + ">" +
-      monthArray.slice(i, i + 2)[0] +
-      "</span>" +
-      "<span id = " + monthArray.slice(i, i + 2)[1] + ">" +
-      monthArray.slice(i, i + 2)[1] +
-      "</span>" +
-      "</div>");
-    i += 2;
-  }
-
-  //默认选中当前月份
-  var curMonth = monthArray[date.getMonth()]
-  $("#" + curMonth).addClass("curMonth");
-
-  //初始化星期
-  var week = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
-  week.forEach(function(item, index, array) {
-    $(".week").append("<div>" + item + "</div>")
-  })
-})()
-
-/**
- * 获取日历显示数据
- * @param  {[type]} year     [description]
- * @param  {[type]} curMonth [description]
- * @return {[type]}          [description]
- */
-function showCalendarData(year, curMonth) {
-  var days = getDaysOfCurMonth(year, curMonth);
-  var firstDayOfWeek = getCurMonthFirstDayOfWeek(year, curMonth);
-  var lastDayOfWeek = getCurMonthLastDayOfWeek(year, curMonth);
-  var preDays = [],
-    curDays = [],
-    lastDays = [],
-    showData = [];
-  firstDayOfWeek != 0 ? preDays = getPreMonth(firstDayOfWeek, year, curMonth) : ""
-  lastDayOfWeek != 6 ? lastDays = getAfterMonth(lastDayOfWeek, year, curMonth) : ""
-  curDays = getCurMonth(days, year, curMonth);
-  showData = groupData(preDays.concat(curDays).concat(lastDays))
-  showData.forEach(function(item) {
-    var html = "<div class = 'aWeek'>"
-    item.forEach(function(item1) {
-      html += "<div class = 'aDay'>" + item1.day + "</div>"
+Calendar.prototype = {
+  // 初始化日历显示
+  _init: function(options) {
+    this.config = $.extend(this.config, options || {})
+    this._renderYear();
+    this._renderMonth();
+    this._renderWeek();
+    this._initializeCurMonth();
+    return this;
+  },
+  // 初始化左侧年份显示
+  _renderYear: function() {
+    for (var i = this.config.endYear; i > this.config.startYear; i--) {
+      $(this.config.eleYear).append("<option value=" + i + ">" + i + "</option>")
+    }
+  },
+  // 初始化左侧月份显示
+  _renderMonth: function() {
+    for (var i = 0; i < this.config.constantMonth.length;) {
+      $(this.config.eleMonth).append("<div>" +
+        "<div id = month" + i + ">" + this.config.constantMonth.slice(i, i + 2)[0] + "</div>" +
+        "<div id = month" + (i + 1) + ">" + this.config.constantMonth.slice(i, i + 2)[1] + "</div>" +
+        "</div>");
+      i += 2;
+    }
+  },
+  // 初始化右侧顶部周显示
+  _renderWeek: function() {
+    this.config.constantWeek.forEach(function(item, index, array) {
+      $(this.config.eleWeek).append("<div>" + item + "</div>")
+    }, this)
+  },
+  // 初始化当前展示月份标记
+  _initializeCurMonth: function() {
+    $("#month" + new Date().getMonth()).addClass("curMonth");
+  },
+  // 获取需要展示的当月信息
+  getCurMonth: function(year, month) {
+    var curMonthArray = [];
+    for (var i = 0; i < this.config.timeCardUtil.getDaysOfCurMonth(year, month); i++) {
+      curMonthArray.push(this.config.timeCardUtil.dateInfo(year, month, i + 1));
+    }
+    return curMonthArray;
+  },
+  // 获取需要展示的上月信息
+  getPreMonth: function(week, year, month) {
+    var preYear, preMonth, days, preMonthArray = [];
+    if (month > 0) {
+      preYear = parseInt(year);
+      preMonth = month - 1;
+    } else {
+      preYear = parseInt(year) - 1;
+      preMonth = 11;
+    }
+    days = this.config.timeCardUtil.getDaysOfCurMonth(preYear, preMonth);
+    for (var i = 0; i < week; i++) {
+      preMonthArray.push(this.config.timeCardUtil.dateInfo(preYear, preMonth, days - i));
+    }
+    return preMonthArray.reverse();
+  },
+  // 获取需要展示的下月信息
+  getAfterMonth: function(week, year, month) {
+    var afterYear, afterMonth, days, afterMonthArray = [];
+    if (month < 11) {
+      afterYear = parseInt(year);
+      afterMonth = month + 1;
+    } else {
+      afterYear = parseInt(year) + 1;
+      afterMonth = 0;
+    }
+    days = this.config.timeCardUtil.getDaysOfCurMonth(afterYear, afterMonth);
+    for (var i = 0; i < 6 - week; i++) {
+      afterMonthArray.push(this.config.timeCardUtil.dateInfo(afterYear, afterMonth, i + 1))
+    }
+    return afterMonthArray;
+  },
+  // 画图方法
+  drawCalendar: function(year, month) {
+    this.date = new Date(year, month);
+    this.clearOldData();
+    // 获取当月第一天是星期几
+    this.firstDayOfWeek = this.config.timeCardUtil.getCurMonthFirstDayOfWeek(year, month);
+    // 获取当月最后一天是星期几
+    this.lastDayOfWeek = this.config.timeCardUtil.getCurMonthLastDayOfWeek(year, month);
+    this.firstDayOfWeek != 0 ? this.preDateInfo = this.getPreMonth(this.firstDayOfWeek, year, month) : ""
+    this.lastDayOfWeek != 6 ? this.afterDateInfo = this.getAfterMonth(this.lastDayOfWeek, year, month) : ""
+    this.curDateInfo = this.getCurMonth(year, month);
+    this.drawHtml();
+  },
+  //  清空日历区DOM和日历对象当月，前一月，后一月对象信息
+  clearOldData: function() {
+    this.preDateInfo = [];
+    this.curDateInfo = [];
+    this.afterDateInfo = [];
+    $("#calendar").empty();
+  },
+  // 拼接日历展示区HTML和添加对应央视
+  drawHtml: function() {
+    var _this = this;
+    var totalArray = this.preDateInfo.concat(this.curDateInfo).concat(this.afterDateInfo);
+    this.config.timeCardUtil.groupData(totalArray).forEach(function(everyGroup) {
+      var html = "<div class = 'aWeek'>"
+      everyGroup.forEach(function(date) {
+        var flag = "";
+        if (date.date.getMonth() != _this.date.getMonth()) {
+          flag = " notCurMonth"
+        }
+        html += "<div class = 'aDay" + flag + " aDay" + date.week + "'><div><u>" + date.day + "</u></div><div>" + date.weekDesc + "</div></div>"
+      })
+      html += "</div>"
+      $("#calendar").append(html);
     })
-    html += "</div>"
-    $("#calendar").append(html);
+    $(".aDay0").addClass('weekend');
+    $(".aDay6").addClass('weekend');
+    $(".aDay0 > div").last().addClass('redColor');
+    $(".aDay6 > div").last().addClass('redColor');
+    $(".notCurMonth > div").last().removeClass('redColor');
+  }
+}
+
+$(function() {
+  // 初始化日历
+  var calender = new Calendar()._init({
+    timeCardUtil: new TimeCardUtil()
+  })
+  calender.drawCalendar(calender.date.getFullYear(), calender.date.getMonth());
+
+  // 年份切换事件
+  $(calender.config.eleYear).bind("change", function() {
+    calender.drawCalendar($(calender.config.eleYear).val(), calender.config.constantMonth.indexOf($(".curMonth").text()));
   })
 
-}
-
-/**
- * 获取当月数据
- * @param  {[type]} curDays  [description]
- * @param  {[type]} year     [description]
- * @param  {[type]} curMonth [description]
- * @return {[type]}          [description]
- */
-function getCurMonth(curDays, year, curMonth) {
-  var curMonthArray = [];
-  for (var i = 0; i < curDays; i++) {
-    curMonthArray.push(dateInfo(year, monthArray.indexOf(curMonth), i + 1));
+  // 月份点击事件
+  for (var i = 0; i < 12; i++) {
+    $("#month" + i).bind("click", function(el) {
+      $(".curMonth").removeClass();
+      $(this).addClass("curMonth");
+      calender.drawCalendar($(calender.config.eleYear).val(), calender.config.constantMonth.indexOf($(".curMonth").text()));
+    })
   }
-  return curMonthArray;
-}
-
-/**
- * 获取当月往前弥补数据
- * @param  {[type]} week     [description]
- * @param  {[type]} year     [description]
- * @param  {[type]} curMonth [description]
- * @return {[type]}          [description]
- */
-function getPreMonth(week, year, curMonth) {
-  var preYear, preMonth;
-  if (monthArray.indexOf(curMonth) > 0) {
-    preYear = parseInt(year);
-    preMonth = monthArray.indexOf(curMonth) - 1;
-  } else {
-    preYear = parseInt(year) - 1;
-    preMonth = 11;
-  }
-  var days = getDaysOfCurMonth(preYear, preMonth);
-  var preMonthArray = []
-  for (var i = 0; i < week; i++) {
-    preMonthArray.push(dateInfo(preYear, preMonth, days - i));
-  }
-  return preMonthArray.reverse();
-}
-
-/**
- * 获取当月往后弥补数据
- * @param  {[type]} week     [description]
- * @param  {[type]} year     [description]
- * @param  {[type]} curMonth [description]
- * @return {[type]}          [description]
- */
-function getAfterMonth(week, year, curMonth) {
-  var afterYear, afterMonth;
-  if (monthArray.indexOf(curMonth) < 11) {
-    afterYear = parseInt(year);
-    afterMonth = monthArray.indexOf(curMonth) + 1;
-  } else {
-    afterYear = parseInt(year) + 1;
-    afterMonth = 0;
-  }
-  var days = getDaysOfCurMonth(afterYear, afterMonth);
-  var afterMonthArray = []
-  for (var i = 0; i < 6 - week; i++) {
-    afterMonthArray.push(dateInfo(afterYear, afterMonth, i + 1))
-  }
-  return afterMonthArray;
-}
-
-/**
- * 通用返回某日详细信息对象
- * @param  {[type]} year  [description]
- * @param  {[type]} month [description]
- * @param  {[type]} day   [description]
- * @return {[type]}       [description]
- */
-function dateInfo(year, month, day) {
-  var date = new Date(year, month, day);
-  var month = date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1
-  var day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate()
-  return {
-    day: month + "-" + day,
-    week: date.getDay(),
-    date: date
-  }
-}
-
-/**
- * 对获取到的日期显示数据按周进行分组
- * @param  {[type]} data [description]
- * @return {[type]}      [description]
- */
-function groupData(data) {
-  var size = data.length / 7;
-  var groupData = [];
-  for (var i = 0; i < size; i++) {
-    groupData.push(data.slice(i * 7, i * 7 + 7))
-  }
-  return groupData;
-}
-
-showCalendarData($("#selectYear").val(), $(".curMonth").text())
+});
--- utils.js
+++ utils.js
@@ -1,58 +1,61 @@
-var monthArray = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
-/**
- * 判断闰年
- * @param  {[type]}  year [description]
- * @return {Boolean}      [description]
- */
-function isLeapYear(year) {
-  return (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
-}
-
-/**
- * 获取当前月份天数
- * @param  {[type]} year  [description]
- * @param  {[type]} month [description]
- * @return {[type]}       [description]
- */
-function getDaysOfCurMonth(year, month) {
-  var days = 0;
-  var _31Days = [1, 3, 5, 7, 8, 10, 12];
-  var is31Days = _31Days.some(function(item) {
-    return monthArray[item - 1] === month
-  })
-  var leapYear = isLeapYear(year);
-  if (is31Days) {
-    days = 31
-  } else {
-    if (month === "Feb") {
-      leapYear ?
-        days = 29 : days = 28
-    } else {
-      days = 30
-    }
+// 日历帮助对象
+function TimeCardUtil() {
+  this.config = {
+    _31Days: [1, 3, 5, 7, 8, 10, 12]
   }
-  return days;
 }
 
-/**
- * 获取当前月份第一天是星期几
- * @param  {[type]} year  [description]
- * @param  {[type]} month [description]
- * @return {[type]}       [description]
- */
-function getCurMonthFirstDayOfWeek(year, month) {
-  var firstDate = new Date(year, monthArray.indexOf(month), 1);
-  return firstDate.getDay();
-
-}
-
-/**
- * 获取当前月份最后一天是星期几
- * @param  {[type]} year  [description]
- * @param  {[type]} month [description]
- * @return {[type]}       [description]
- */
-function getCurMonthLastDayOfWeek(year, month) {
-  var lastDate = new Date(year, monthArray.indexOf(month), getDaysOfCurMonth(year, month));
-  return lastDate.getDay();
+TimeCardUtil.prototype = {
+  // 判断闰年
+  leapYear: function(year) {
+    return (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
+  },
+  // 获取某年某月有多少天
+  getDaysOfCurMonth: function(year, month) {
+    var days = 0;
+    var is31Days = this.config._31Days.some(function(item) {
+      return item - 1 === month
+    }, this)
+    var isLeapYear = this.leapYear(year);
+    if (is31Days) {
+      days = 31
+    } else {
+      if (month === 1) {
+        isLeapYear ?
+          days = 29 : days = 28
+      } else {
+        days = 30
+      }
+    }
+    return days;
+  },
+  // 获取当月第一天是星期几
+  getCurMonthFirstDayOfWeek: function(year, month) {
+    return new Date(year, month, 1).getDay();
+  },
+  // 获取当月最后一天是星期几
+  getCurMonthLastDayOfWeek: function(year, month) {
+    return new Date(year, month, this.getDaysOfCurMonth(year, month)).getDay();
+  },
+  // 返回日历中的天的详细对象信息
+  dateInfo: function(year, month, day) {
+    var date = new Date(year, month, day);
+    var month = date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1
+    var day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate()
+    return {
+      day: month + "-" + day,
+      week: date.getDay(),
+      date: date,
+      weekDesc: date.getDay() === 0 || date.getDay() === 6 ? "休息日" : "工作日"
+    }
+  },
+  // 分组应该显示的日历集合
+  groupData: function(dateArray) {
+    var size = dateArray.length / 7;
+    var groupData = [];
+    for (var i = 0; i < size; i++) {
+      groupData.push(dateArray.slice(i * 7, i * 7 + 7))
+    }
+    return groupData;
+  }
 }
